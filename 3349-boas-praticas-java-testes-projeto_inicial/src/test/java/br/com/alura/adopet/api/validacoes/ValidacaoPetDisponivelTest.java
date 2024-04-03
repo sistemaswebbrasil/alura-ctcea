28d2a235c6346e82057b99008bb9a70f7fd421be
sistemaswebbrasil <adriano.faria@outlook.com.br>
2024-04-03 18:45:32 -0300

    https://cursos.alura.com.br/course/boas-praticas-programacao-testes-java/task/138351

--- 3349-boas-praticas-java-testes-projeto_inicial/src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoPetDisponivelTest.java
+++ 3349-boas-praticas-java-testes-projeto_inicial/src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoPetDisponivelTest.java
@@ -1,10 +1,13 @@
 package br.com.alura.adopet.api.validacoes;
 
 import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.Pet;
 import br.com.alura.adopet.api.repository.PetRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -20,18 +23,36 @@ class ValidacaoPetDisponivelTest {
     @Mock
     private PetRepository petRepository;
 
+    @Mock
+    private Pet pet;
+
+    @Mock
+    private SolicitacaoAdocaoDto dto;
+
     @Test
     void deveriaPermitirSolicitacaoDeAdocaoPet() {
 
         // ARRANGE
-        SolicitacaoAdocaoDto dto = new SolicitacaoAdocaoDto(
-                7l,
-                2l,
-                "Motivo qualquer"
-        );
+        BDDMockito.given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        BDDMockito.given(pet.getAdotado()).willReturn(false);
+
+
 
         //ASSERT + ACT
         Assertions.assertDoesNotThrow(() -> validacao.validar(dto));
+    }
+
+    @Test
+    void NaoDeveriaPermitirSolicitacaoDeAdocaoPet() {
+
+        // ARRANGE
+        BDDMockito.given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        BDDMockito.given(pet.getAdotado()).willReturn(true);
+
+
+
+        //ASSERT + ACT
+        Assertions.assertThrows(ValidacaoException.class,() -> validacao.validar(dto));
     }
 
 }
