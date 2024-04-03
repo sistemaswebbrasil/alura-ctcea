b7998108a42202413a931c0eac5f5e4bdea818e0
sistemaswebbrasil <adriano.faria@outlook.com.br>
2024-04-03 06:26:17 -0300

    https://cursos.alura.com.br/course/boas-praticas-programacao-testes-java/task/138350

--- 3349-boas-praticas-java-testes-projeto_inicial/src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoPetDisponivelTest.java
+++ 3349-boas-praticas-java-testes-projeto_inicial/src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoPetDisponivelTest.java
@@ -1,12 +1,24 @@
 package br.com.alura.adopet.api.validacoes;
 
 import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.repository.PetRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class ValidacaoPetDisponivelTest {
+
+    @InjectMocks
+    ValidacaoPetDisponivel validacao;
+
+    @Mock
+    private PetRepository petRepository;
 
     @Test
     void deveriaPermitirSolicitacaoDeAdocaoPet() {
@@ -17,8 +29,6 @@ class ValidacaoPetDisponivelTest {
                 2l,
                 "Motivo qualquer"
         );
-
-        ValidacaoPetDisponivel validacao = new ValidacaoPetDisponivel();
 
         //ASSERT + ACT
         Assertions.assertDoesNotThrow(() -> validacao.validar(dto));
